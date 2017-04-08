7aac2f9638df6384444bff4627ba3da86f9e80f8
manojk16 <manoj.kumar5@kpit.com>
2017-04-08 14:42:17 +0530

    Private Embedded Object

--- inheritance/src/useful.cpp
+++ inheritance/src/useful.cpp
@@ -4,22 +4,27 @@ using namespace std;
 
 class Y{
 int i;
+X x;//embedded Object or composition, generally Embedded Object is Private 
 public:
 Y(){
 	i=0;
 }
-X x;//embedded Object or composition
+
 void f(int ii){
 	i=ii;
+	x.set(ii);
 }
 int g() const{
-	return i;
-}
 
+	return i*x.read();
+}
+void permute(){
+	x.premute();
+}
 };
 int main(){
 	Y y;
 	y.f(47);
-	y.x.set(37);
+	y.permute();
 }
 
