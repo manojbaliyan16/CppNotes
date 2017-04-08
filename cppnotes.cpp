be4f443688d20356b9a117d48e16f560e8cb6974
manojk16 <manoj.kumar5@kpit.com>
2017-04-08 14:55:39 +0530

    Adding the Notes

---
+++ cppnotes.cpp
@@ -0,0 +1,4 @@
+Composition:
+You simply create objects of your existing class inside the new class. This is called composition because the new class is
+Composed of objects of existing classes.
+Y contains a subobject of X: As per useful.cpp  Both member objects and base class storage are referred to as subobjects.
