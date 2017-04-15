9234ab6e89596dca54d154e473b362ea60ded8b3
manojk16 <manoj.kumar5@kpit.com>
2017-04-15 15:05:26 +0530

    Addition of Exercise1

--- inheritance/src/car.cpp
+++ inheritance/src/car.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
-
+#include <string>
 
 using namespace std;
 
-
+class Vechile{
+	string brand;
+public:
+	Vechile(string name):brand(name){}
+	string get(){
+		cout <<"Brand is::"<< brand <<endl;
+		return brand;
+	}
+};
 class Engine{
 public:
 	void start() const{}
@@ -26,8 +34,13 @@ public:
 	void open() const{}
 	void close() const{} 
 };
-class Car{
+class Car:public Vechile{
+	
+
 public:
+	Car():Vechile(""){
+		cout <<"constructor Called"<<endl;
+	}
 	Engine engine;
 	Wheel wheel[4];
 	Door left, right;
@@ -36,6 +49,8 @@ int main(){
 	Car car;
 	car.left.window.rollup();
 	car.wheel[0].inflate(72);
+	Vechile v("BMW");
+	string s=v.get();
 }//Here to use composition is good makein meber public will help the client how to use the car and require less less code complexit
 // Here no sense to make car is a vechile object beacuse car doesn't contain the vechile 
 //The is a relationship is expressed with Inheritance and Has a relationship is expresse with Composition
