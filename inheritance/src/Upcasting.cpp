4e2cc82af00631fe3ef756038fc352fc20305a98
manojk16 <manoj.kumar5@kpit.com>
2017-04-15 16:07:11 +0530

    Problem With Out Virtual Function

--- inheritance/src/Upcasting.cpp
+++ inheritance/src/Upcasting.cpp
@@ -3,10 +3,17 @@ using namespace std;
 enum note{MiddleC, CSharp, Cflat};
 class Instrument{
 public:
-	void play(note) const{}
+	void play(note) const{
+		cout<<"Instrument Play Note"<<endl;
+	}
 };
 
-class Wind : public Instrument{};
+class Wind : public Instrument{
+public:
+	void play(note) const{
+		cout<<"Wind Play Note"<<endl;
+	}
+};
 
 void Tune (Instrument& i){
 
@@ -20,3 +27,12 @@ int main(){
 	//the code works for Instrument and anything derived from Instrument, and the act of converting a Wind
 	 //reference or pointer into an Instrument reference or pointer is called upcasting.
 }
+/*The function tune( ) accepts (by reference) an Instrument, but also without complaint anything derived from Instrument. 
+In main( ),you can see this happening as a Wind object is passed to tune( ), with no cast necessary. This is acceptable; the interface 
+ in Instrument must exist in Wind, because Wind is publicly inherited from Instrument. Upcasting from Wind to Instrument may 
+ “narrow” that interface, but never less than the full interface to Instrument.
+
+The same arguments are true when dealing with pointers; the only difference is that the user must explicitly take the addresses 
+ts as they are passed into the function.*/
+/*here we are passing object of wind to call the function Tune but 
+Here Problem is only: Instrument::play() is called, So To Understand this issue we have to under stand concept of binding */
