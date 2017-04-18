da977bedcc5feaab7f1b27cd0a401b692848fda4
manojk16 <mkumar9824@gmail.com>
2017-04-19 01:30:08 +0530

    Exercise 1 Updated

--- Polymorphism-VirFunc/Exercise1.cpp
+++ Polymorphism-VirFunc/Exercise1.cpp
@@ -11,30 +11,39 @@ virtual void draw() const{
 
 class Circle:public Shape{
 public:
-	void draw() {
+	void draw() const{
 		cout<<"Drawn Circle"<<endl;
 	}
 };
 class Square:public Shape{
 	public:
-	void draw() {
+	void draw() const{
 		cout<<"Drawn Square"<<endl;
 	}
 };
 class Traingle:public Shape{
 	public:
-	void draw() {
+	void draw() const{
 		cout<<"Drawn Traingle"<<endl;
 	}
 };
 	
 int main(){
-Shape* A[] = { new Circle,new Square,new Traingle};
-//Shape* A= new Circle;
-Circle* c1=dynamic_cast<Circle*>(A[0]);
-Square* c2=dynamic_cast<Square*>(A[1]);
-Traingle* c3=dynamic_cast<Traingle*>(A[2]);
-c1->draw();
-c2->draw();
-c3->draw();
+/*Shape* A[] = { new Circle,new Square,new Traingle};
+
+for(int i=0;i<3;i++){
+	A[i]->draw();
+}
+*/
+
+	//Circle c1;
+	//Square s1;
+	//Traingle t1;
+	//Shape* bp[] = {&c1,&s1,&t1};
+Shape* bp[] = { new Circle,new Square, new Traingle};
+
+for(int i=0;i<3;i++)
+{
+	bp[i]->draw();
+}
 }	
