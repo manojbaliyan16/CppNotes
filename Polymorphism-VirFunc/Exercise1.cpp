2f1bb38ec7aed73f961b955c652ac964cf4f27aa
manojk16 <mkumar9824@gmail.com>
2017-04-19 00:05:07 +0530

    Ex-1 Completed and OperatorPolymorphism

--- Polymorphism-VirFunc/Exercise1.cpp
+++ Polymorphism-VirFunc/Exercise1.cpp
@@ -1,32 +1,40 @@
 #include <iostream>
+using namespace std;
 
 class Shape{
 public:
 virtual void draw() const{
-	cout << "Draw the shape">>endl;
+	cout << "Draw the shape"<<endl;
 }
 
 };
 
 class Circle:public Shape{
 public:
-	void draw() const{
+	void draw() {
 		cout<<"Drawn Circle"<<endl;
 	}
 };
 class Square:public Shape{
 	public:
-	void draw() const{
+	void draw() {
 		cout<<"Drawn Square"<<endl;
 	}
 };
 class Traingle:public Shape{
 	public:
-	void draw() const{
+	void draw() {
 		cout<<"Drawn Traingle"<<endl;
 	}
 };
 	
 int main(){
-	
+Shape* A[] = { new Circle,new Square,new Traingle};
+//Shape* A= new Circle;
+Circle* c1=dynamic_cast<Circle*>(A[0]);
+Square* c2=dynamic_cast<Square*>(A[1]);
+Traingle* c3=dynamic_cast<Traingle*>(A[2]);
+c1->draw();
+c2->draw();
+c3->draw();
 }	
