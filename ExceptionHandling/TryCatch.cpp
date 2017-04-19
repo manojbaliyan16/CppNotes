6a270c8b1e1871d32725367820948044e8e71ffc
manojk16 <mkumar9824@gmail.com>
2017-04-19 23:57:20 +0530

    TryCatche.cpp Added

--- ExceptionHandling/TryCatch.cpp
+++ ExceptionHandling/TryCatch.cpp
@@ -1,2 +1,24 @@
 #include <iostream>
 using namespace std;
+	
+	class Rainbow{
+	public:
+		Rainbow(){}
+		~Rainbow(){}
+	};
+
+	void oz(){
+		Rainbow rb;
+		for(int i=0;i<3;i++)
+			cout<<"There is no place like Home\n";
+		throw 47;
+	}
+	int main(){
+		try{
+			cout<<"Delhi, Banglore, Pune\n";
+			oz();
+		}
+		catch(int){
+			cout<<"I had Stranges Dream\n";
+		}
+	}
