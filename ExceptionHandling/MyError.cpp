a63f07e9051a36dd61a8a36bd62031227463e606
manojk16 <mkumar9824@gmail.com>
2017-04-19 23:41:06 +0530

    ExceptionHAndling

---
+++ ExceptionHandling/MyError.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class MyError{
+const char* const data;
+public:
+	MyError(const char* const msg=0):data(msg){}
+};
+void f(){
+	throw  MyError("SomeThing Bad Happened");
+}
+
+int main(){
+	try {
+		cout<<"In Try\n";
+		f();
+	}
+	catch(MyError){
+		cout<<"Exception Catched\n";
+	}
+}
