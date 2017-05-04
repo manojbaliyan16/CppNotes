df25d4046bcecaf46f6a17be5d3c97dc12c93e81
Manoj Kumar <mkumar9824@gmail.com>
2017-05-04 13:24:08 +0530

    Updated with Why [] overloading Require

--- Templates/TemplateClass_Array.cpp
+++ Templates/TemplateClass_Array.cpp
@@ -1,60 +1,59 @@
-// With this example we will come to know, What is the use of Template Class 
-// Here with single code I can create an Ineger array and and Double Array due to Template class 
-#include <iostream>
-#include <assert.h>
+	#include <iostream>
+	#include <assert.h>
 
-#ifndef ARRAY_H
-#define ARRAY_H
+	#ifndef ARRAY_H
+	#define ARRAY_H
 
-template <class T>
-class Array{
-private:
-	int m_length;
-	T* m_data;
-public:
-	Array(){
-		m_length=0;
-		m_data=0;
+	template <class T>
+	class Array{
+	private:
+		int m_length;
+		T* m_data;
+	public:
+		Array(){
+			m_length=0;
+			m_data=0;
+		}
+		Array(int length){
+			m_length=length;
+			m_data=new T[length];
+		}
+		~Array(){
+			delete [] m_data;
+		}
+		void erase(){
+			delete [] m_data;
+			m_data=0;
+			m_length=0;
+		}
+		//Here Use of [] operator overloding is bcz we have to use with intarray[] and doublearray [] which is object of template class 
+		T& operator[](int index){
+			assert(index >= 0 && index < m_length);
+			return m_data[index];
+		}
+		int getlength();
+	};
+	//memeber function defined uotside the class needs its own template and note that class name is Array<T> not Array
+	//because Array would refer to not templated version of the class and Array<T>
+	template <typename T>
+	int Array<T>::getlength(){
+		return m_length;
 	}
-	Array(int length){
-		m_length=length;
-		m_data=new T[length];
-	}
-	~Array(){
-		delete [] m_data;
-	}
-	void erase(){
-		delete [] m_data;
-		m_data=0;
-		m_length=0;
-	}
-	T& operator[](int index){
-		assert(index >= 0 && index < m_length);
-		return m_data[index];
-	}
-	int getlength();
-};
-//memeber function defined uotside the class needs its own template and note that class name is Array<T> not Array
-//because Array would refer to not templated version of the class and Array<T>
-template <typename T>
-int Array<T>::getlength(){
-	return m_length;
-}
 
-#endif
+	#endif
 
-int main(int argc, char const *argv[])
-{
-	Array<int> intArray(12);
-	Array<double> doubleArray(14);
-	for (int count = 0;count<intArray.getlength(); ++count)
+	int main(int argc, char const *argv[])
 	{
-		intArray[count]=count;
-		doubleArray[count]=count+0.5;
+		Array<int> intArray(12);
+		Array<double> doubleArray(14);
+		for (int count = 0;count<intArray.getlength(); ++count)
+		{
+			intArray[count]=count;
+			doubleArray[count]=count+0.5;
+		}
+		for (int count = intArray.getlength()-1 ;count >=0; --count)
+		{
+			std::cout<< intArray[count] << " \t " << doubleArray[count] <<std::endl;
+		}
+		return 0;
 	}
-	for (int count = intArray.getlength()-1 ;count >=0; --count)
-	{
-		std::cout<< intArray[count] << " \t " << doubleArray[count] <<std::endl;
-	}
-	return 0;
-}
