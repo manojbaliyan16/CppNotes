cb2c9fc177d0ea219f7c58e07ee3bec60584ec4e
manojk16 <mkumar9824@gmail.com>
2017-05-13 00:59:13 +0530

    ch1 Effective CPP

--- cppNotes.cpp
+++ cppNotes.cpp
@@ -106,3 +106,63 @@ one of the fundamental way to improve the c++ performance is to increase is the 
 it is good that never modify return value of function which return built in type
 
 
+Things to Remember:
+Declaring something const helps compilers detect usage errors. const
+can be applied to objects at any scope, to function parameters and
+return types, and to member functions as a whole.
+✦ Compilers enforce bitwise constness, but you should program using
+logical constness.
+✦ When const and non-const member functions have essentially identical implementations, code duplication can be avoided by having the
+non-const version call the const version.Declaring something const helps compilers detect usage errors. const
+can be applied to objects at any scope, to function parameters and
+return types, and to member functions as a whole.
+✦ Compilers enforce bitwise constness, but you should program using
+logical constness.
+✦ When const and non-const member functions have essentially identical implementations, code duplication can be avoided by having the
+non-const version call the const version.
+Item 4:
+make sure that object is initialized before they used :
+the reponsibility of initilization is falls on the constructor 
+class phoneNumber{};
+class ABEntry{
+	public:
+	ABEntry(const string& name, const string& address, list<phoneNumber>&Phones)
+private:
+	string TheName;
+	string TheAddress;
+	list<phoneNumber> ThePhones;
+	int NumTimesCalculated;
+};
+ABEntry::ABEntry(const string& name, const string& address, list<phoneNumber>&Phones){
+	TheName=name;
+	TheAddress=address;
+	ThePhones=Phones;
+	NumTimesCalculated=0;
+	//These are all assignments not the Initialization 
+	//no need to be confus between Initialization and assignments, this is not the best approach as C++ says
+	// that datamember of the objects shall be initialized before entring in to the body of the constructor and in 
+	the body of ABEntry this is the assignments while initilization already happened when default constrctor is called 
+	but this is not true for NumTimesCalculated because it is built in type so there is no garaunttee that this already 
+	initilaed or not so a better way to use member initialization list
+	ABEntry::ABEntry(const string& name, const string& address, list<phoneNumber>&phones):TheName(name),TheAddress(address),ThePhones(Phones),
+	NumTimesCalculated(0){} this is initialization.
+}
+if any class store the reference of object passed to the constructor arguments or class have const variable then it is mandotory 
+to use the constructor initilization list.
+Other reson to Use member initializ list 
+1. Base class Initialization:
+As base class should be constructed before the child class so if the base class have default construtor then there 
+is no problem but if base class doesn't have any defaault construtor then it is mandatory to use member initilize 
+list in to parameterize of the base class to ensure that ur base class is initialized before the child class 
+2. Initialization of subobjects which only have parameterized construtor 
+3. efficiency 
+4. Initialization of non static data members
+5. Initialization of reference data member : Reference datamember should be initialized where they are declared 
+and it is possible with member initialization list.
+
+Static Object: A static Object is one which is exist from the time it is constructed till the program ends
+static object declared inside the function is called local static object bcz this scope with in the function itself 
+static object distructor is called when main finishes its execution. 
+
+Chapter 2:	
+Constructor, distructor & Assignment operator:
