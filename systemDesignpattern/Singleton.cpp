ad890e59e681324c09ce339add0658aa0ba40b56
manojk16 <mkumar9824@gmail.com>
2017-06-11 10:57:03 +0530

    Singleton DP

---
+++ systemDesignpattern/Singleton.cpp
@@ -0,0 +1,48 @@
+/*
+ * Singleton.cpp
+
+ *
+ *  Created on: 09-Jun-2017
+ *      Author: baliyan
+ */
+#include <iostream>
+using namespace std;
+class Singleton {
+	Singleton() {
+	}
+	;
+	static Singleton *single;
+	static bool IsSingle;
+public:
+
+	static Singleton* getInsatnce();
+
+	void method();
+	~Singleton() {
+		IsSingle = false;
+	}
+
+};
+bool Singleton::IsSingle = false;
+Singleton* Singleton::single = NULL;
+Singleton* Singleton::getInsatnce() {
+	single = new Singleton();
+	if (!IsSingle) {
+		IsSingle = true;
+		return single;
+	} else
+		return single;
+}
+void Singleton::method() {
+	cout << "Singleton Method Called \n";
+}
+
+int main() {
+
+	Singleton *ptr1, *ptr2;
+	ptr1 = Singleton::getInsatnce();
+	ptr1->method();
+	ptr2 = Singleton::getInsatnce();
+	ptr2->method();
+
+}
